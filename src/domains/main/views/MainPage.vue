7d748a604d25c84dce253425ffb90ee75b628ed8
dikalee7 <dikalee7@gmail.com>
2023-04-04 17:27:15 +0900

    vue-chartjs 설치

--- src/domains/main/views/MainPage.vue
+++ src/domains/main/views/MainPage.vue
@@ -1,4 +1,5 @@
 <template>
+  <Bar :data="data" :options="options" />
   <v-sheet class="mx-auto">
     <v-form @submit.prevent="submitForm">
       <v-btn type="submit" block class="mt-2">Submit</v-btn>
@@ -13,13 +14,41 @@
 import { defineComponent } from 'vue';
 import { publicApis } from '@/api/index';
 import { IFBillInfo } from '../type';
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js';
+import { Bar } from 'vue-chartjs';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
 export default defineComponent({
+  components: { Bar },
   setup() {
     return {};
   },
   data() {
     return {
       rsltJson: '',
+      data: {
+        labels: ['January', 'February', 'March'],
+        datasets: [{ data: [40, 20, 12] }],
+      },
+      options: {
+        responsive: true,
+      },
     };
   },
   methods: {
