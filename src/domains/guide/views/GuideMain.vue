f2fb299a4dec7c37eaeda8a3c500db636aab671f
dikalee7 <dikalee7@gmail.com>
2023-04-13 14:36:05 +0900

    탭 컨텐츠 영역 드레그시 탭 이동 되지 않도록 수정

--- src/domains/guide/views/GuideMain.vue
+++ src/domains/guide/views/GuideMain.vue
@@ -1,16 +1,12 @@
 <template>
   <v-card color="basil">
-    <!-- <v-card-title class="text-center justify-center py-6">
-      <h1 class="font-weight-bold text-h4 text-basil">학습 가이드</h1>
-    </v-card-title> -->
-
     <v-tabs v-model="tab" bg-color="transparent" color="basil" grow>
       <v-tab v-for="item in items" :key="item.id" :value="item.id">
         {{ item.name }}
       </v-tab>
     </v-tabs>
 
-    <v-window v-model="tab">
+    <v-window v-model="tab" disabled>
       <v-window-item value="tab1">
         <v-card color="basil" flat>
           <DistGuide />
@@ -57,8 +53,5 @@ export default defineComponent({
 /* Helper classes */
 .bg-basil {
   background-color: #fffbe6 !important;
-}
-.text-basil {
-  color: #356859 !important;
 }
 </style>
