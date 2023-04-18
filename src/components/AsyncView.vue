06952354f3f18d0a50abe5987c1439e04aad094b
dikalee7 <dikalee7@gmail.com>
2023-04-19 01:42:55 +0900

    async component 추가

---
+++ src/components/AsyncView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div><component :is="loadedComp" /></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, defineAsyncComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    comp: {
+      type: String,
+    },
+  },
+  setup(props) {
+    const loadedComp = computed(() => {
+      return defineAsyncComponent(() => import(`${props.comp}`));
+    });
+    return { loadedComp };
+  },
+});
+</script>
+
+<style scoped></style>
