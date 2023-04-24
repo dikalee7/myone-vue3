e8c60269a448462ecb2a0e8899cf484f0151f53b
dikalee7 <dikalee7@gmail.com>
2023-04-24 16:50:12 +0900

    mitt 적용

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,13 @@ import SelectComp from '@/components/vueti/SelectComp.vue';
 import ModalView from '@/components/ModalView.vue';
 import useCmn from '@/composables/cmn';
 import useMo from '@/composables/mo';
+import mitt from 'mitt';
 
 loadFonts();
 const app = createApp(App);
 app.config.globalProperties.$utils = useCmn();
 app.config.globalProperties.$mo = useMo();
+app.config.globalProperties.$emitter = mitt();
 app.component('SelectComp', SelectComp);
 app.component('ModalView', ModalView);
 app.use(router).use(store).use(vuetify).use(VueSweetalert2).mount('#app');
--- src/runtime.d.ts
+++ src/runtime.d.ts
@@ -1,6 +1,7 @@
 import store from '@/store';
 import { IFUtils } from '@/composables/cmn';
 import { IFMo } from '@/composables/mo';
+import { Emitter } from 'mitt';
 import Swal from 'sweetalert2/dist/sweetalert2.js';
 declare type TVueSwalInstance = typeof Swal & typeof Swal.fire;
 
@@ -10,6 +11,7 @@ declare module '@vue/runtime-core' {
     $store: store;
     $swal: TVueSwalInstance;
     $mo: IFMo;
+    $emitter: Emitter<any>;
   }
 }
 export {};
