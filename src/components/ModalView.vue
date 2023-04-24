77760bc58f4e9f8021f10f5cd65d44e1a79bf46e
dikalee7 <dikalee7@gmail.com>
2023-04-24 23:21:48 +0900

    가이드 수정

--- src/components/ModalView.vue
+++ src/components/ModalView.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <component :is="loadedModal" :cparam="modalInfo.cparam" />
+    <component
+      :is="loadedModal"
+      :cparam="modalInfo.cparam"
+      @modalConfirm.once="fnConfirm"
+      @modalCancel.once="fnCancel"
+    />
   </div>
 </template>
 
@@ -15,6 +20,7 @@ import { useStore } from 'vuex';
 import { IFModal } from '@/store/modules/modalInfo';
 
 export default defineComponent({
+  emits: ['modalConfirm', 'modalCancel'],
   setup() {
     const store = useStore();
     const modalInfo: ComputedRef<IFModal> = computed(
@@ -33,6 +39,14 @@ export default defineComponent({
     });
 
     return { loadedModal, modalInfo };
+  },
+  methods: {
+    fnConfirm(...arg: any) {
+      this.$emit('modalConfirm', ...arg);
+    },
+    fnCancel(...arg: any) {
+      this.$emit('modalConfirm', ...arg);
+    },
   },
 });
 </script>
--- src/domains/guide/components/UtilGuide.vue
+++ src/domains/guide/components/UtilGuide.vue
@@ -1,21 +1,40 @@
 <template>
   <div>
-    <v-card :theme="theme" title="this.$utils.cmn.alert" class="mb-3">
+    <v-card
+      :theme="theme"
+      title="this.$utils.cmn.alert"
+      class="mb-3"
+      v-if="sGuid == 'A' || sGuid == 'AlertUtil'"
+    >
       <v-card-text>알림(alert, confirm) 공통 가이드</v-card-text>
     </v-card>
-    <v-card :theme="theme" title="this.$utils.cmn.setModal" class="mb-3">
+    <v-card
+      :theme="theme"
+      title="this.$utils.cmn.setModal"
+      class="mb-3"
+      v-if="sGuid == 'A' || sGuid == 'ModalUtil'"
+    >
       <v-card-text>동적 팝업 가이드</v-card-text>
     </v-card>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, computed } from 'vue';
 
 export default defineComponent({
-  setup() {
+  props: {
+    selectedGuid: {
+      type: String,
+      default: 'A',
+    },
+  },
+  setup(props) {
     const theme = ref('light');
-    return { theme };
+    const sGuid = computed(() => {
+      return props.selectedGuid;
+    });
+    return { theme, sGuid };
   },
 });
 </script>
