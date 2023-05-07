ca211661165cf29acda297fc7682f423147b832d
dikalee7 <dikalee7@gmail.com>
2023-05-07 17:38:35 +0900

    샘플리스트 작업중

---
+++ src/components/vueti/ListComp.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-list :items="items" item-props lines="three">
+    <template v-slot:subtitle="{ subtitle }">
+      <div v-html="subtitle"></div>
+    </template>
+  </v-list>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    items: {
+      type: Object,
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
--- src/domains/sample/views/SampleList.vue
+++ src/domains/sample/views/SampleList.vue
@@ -4,7 +4,7 @@
       <v-card>
         <PopupHeader :title="title" @closePopup="fnClosePop" />
 
-        <v-card-text class="pa-2"> 작성중 </v-card-text>
+        <ListComp :items="listData" />
       </v-card>
     </v-dialog>
   </v-row>
@@ -12,15 +12,86 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
+import ListComp from '@/components/vueti/ListComp.vue';
+import useUtils from '@/composables/utils';
+import { publicApiCall } from '@/api/index';
+import { IFBillInfoDetail } from '@/api/types/publicApi';
+import { AxiosResponse } from 'axios';
 
 export default defineComponent({
+  components: {
+    ListComp,
+  },
   setup() {
+    const { cmn, useMo } = useUtils();
     const dialog = ref(false);
     const title = ref('리스트 샘플');
+    const listData = ref([]);
 
-    return { title, dialog };
+    return { cmn, mo: useMo(), title, dialog, listData };
+  },
+  mounted() {
+    this.fnInit();
   },
   methods: {
+    fnInit() {
+      this.callBillInfo();
+      this.listData = [
+        { type: 'subheader', title: '의안 목록' },
+        {
+          prependAvatar: '',
+          title: 'Brunch this weekend?',
+          subtitle: `<span class="text-primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+        },
+        { type: 'divider', inset: true },
+        {
+          prependAvatar: '',
+          title: 'Summer BBQ',
+          subtitle: `<span class="text-primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+        },
+        { type: 'divider', inset: true },
+        {
+          prependAvatar: '',
+          title: 'Oui oui',
+          subtitle:
+            '<span class="text-primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+        },
+        { type: 'divider', inset: true },
+        {
+          prependAvatar: '',
+          title: 'Birthday gift',
+          subtitle:
+            '<span class="text-primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+        },
+        { type: 'divider', inset: true },
+        {
+          prependAvatar: '',
+          title: 'Recipe to try',
+          subtitle:
+            '<span class="text-primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+        },
+      ];
+    },
+    async callBillInfo() {
+      const response: AxiosResponse<IFBillInfoDetail[]> = await publicApiCall(
+        '/9710000/BillInfoService2/getBillInfoList',
+        {},
+      );
+
+      this.listData = response.data.reduce((p, c) => {
+        console.log('p==>', p);
+        console.log('c==>', c);
+        p.push(
+          {
+            title: `no.${c.billNo} ${c.proposeDt}`,
+            subtitle: c.billName,
+          },
+          { type: 'divider', inset: false },
+        );
+        return p;
+      }, []);
+      console.log(JSON.stringify(response, null, 2));
+    },
     fnOpenPop() {
       this.dialog = true;
     },
